4221e28f51dc4e8886400c989af28aa2ac86f9bb
Merch7x <aquamaingi@gmail.com>
2024-02-23 22:55:43 +0300

    Remove pk ref

--- models/place.py
+++ models/place.py
@@ -10,9 +10,9 @@ place_amenity = Table(
     'place_amenity',
     Base.metadata,
     Column('place_id', String(60), ForeignKey(
-        'places.id'), nullable=False, primary_key=True),
+        'places.id'), nullable=False),
     Column('amenity_id', String(60), ForeignKey(
-        'amenities.id'), nullable=False, primary_key=True)
+        'amenities.id'), nullable=False,)
 )
 
 
