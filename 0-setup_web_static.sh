42984fca5d34723243a10eb10f6671082692bb88
Merch7x <aquamaingi@gmail.com>
2024-03-07 15:34:28 +0300

    Modify default file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #Installs and configures nginx on a server
 #Install nginx
-# sudo apt update -y
-# sudo apt install -y nginx
+sudo apt update -y
+sudo apt install -y nginx
 
 # #firewall config
-# sudo ufw allow 'Nginx HTTP'
+sudo ufw allow 'Nginx HTTP'
 
 #Configure paths
-sudo mkdir -P /data/web_static/releases/test/
-sudo mkdir -P /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
 #Update permissions
 sudo chown -R "ubuntu":"ubuntu" /data/
@@ -26,12 +26,48 @@ echo "<html>
 #create symbolic links
 ln -s /data/web_static/releases/test  /data/web_static/current
 
+#Remove old config
+sudo rm /etc/nginx/sites-enabled/default
+
 
 #Update nginx.config
-sudo sed -i '0,/server {/a \
-   location \/hbnb_static\/ { \
-      alias /data/web_static/current; \
-   }' /etc/nginx/sites-available/default
+sudo tee "
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        # SSL configuration
+        #
+        # listen 443 ssl default_server;
+        # listen [::]:443 ssl default_server;
+        root /var/www/html;
+
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+        rewrite ^/redirect_me https://www.youtube.com/ permanent;
+        error_page 404 /custom_404.html;
+        location = /custom_404.html {
+            root /usr/share/nginx/html;
+            internal;
+        }
+
+
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files $uri $uri/ =404;
+                add_header X-Served-By "473257-web-01";
+        }
+
+        location = /hbnb_static/ {
+                alias /data/web_static/current/;
+                add_header X-Served-By "473257-web-01";
+        }
+}" > /etc/nginx/sites-available/default
+
+#create new symbolic link
+sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
 #Restart nginx/data/web_static/releases/test
 sudo systemctl restart nginx
