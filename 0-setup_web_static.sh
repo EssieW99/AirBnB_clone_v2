2ba7bb90803aa24b413068347b968978492f686d
Merch7x <aquamaingi@gmail.com>
2024-03-08 08:38:38 +0300

    Remove fault area

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,13 +15,13 @@ sudo mkdir -p /data/web_static/shared/
 sudo chown -R "ubuntu":"ubuntu" /data/
 
 #create index.html file
-echo "<html>
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 #create symbolic links
 ln -s /data/web_static/releases/test  /data/web_static/current
