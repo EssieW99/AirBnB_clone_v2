cf10d9a7c76a0d2dc291e036a8b2a19ac6f44470
Merch7x <aquamaingi@gmail.com>
2024-03-10 21:50:21 +0300

    Fix linter errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,16 +17,18 @@ def do_deploy(archive_path):
     try:
         archive_name = path.basename(archive_path)
         archive_no_ext, ext = path.splitext(archive_name)
-        put("{}".format(archive_path), "/tmp/{}".format(archive_name), mode=755)
+        put("{}".format(archive_path), "/tmp/{}"
+            .format(archive_name), mode=755)
         run("mkdir -p /data/web_static/releases/{}".format(archive_no_ext))
-        sudo(
-            f"tar -xzf /tmp/{archive_name} -C /data/web_static/releases/{archive_no_ext}/")
+        sudo(f"tar -xzf /tmp/{archive_name}\
+              -C /data/web_static/releases/{archive_no_ext}/")
         sudo("mv /data/web_static/releases/{}/web_static/* \
-             /data/web_static/releases/{}".format(archive_no_ext, archive_no_ext))
+             /data/web_static/releases/{}"
+             .format(archive_no_ext, archive_no_ext))
         sudo("rm /tmp/{}".format(archive_name))
         sudo("rm /data/web_static/current")
-        sudo(
-            f"ln -s /data/web_static/releases/{archive_no_ext} /data/web_static/current")
+        sudo(f"ln -s /data/web_static/releases/{archive_no_ext}\
+              /data/web_static/current")
         return True
     except exceptions.CommandTimeout as e:
         return False
