b25f893b02b8b39f7f25a0abb3b21fe4beb3061d
Merch7x <aquamaingi@gmail.com>
2024-03-22 08:11:57 +0300

    Update flask app creation

--- web_flask/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Creates necessary configs for flask app"""
-from flask import Flask
-
-
-def create_app():
-    """Creates a flask app instance"""
-    app = Flask(__name__)
-    return app
