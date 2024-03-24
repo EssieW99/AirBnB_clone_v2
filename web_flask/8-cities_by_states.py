0a34bec5262b0d1485168366bfb2fc1283311e3b
Merch7x <aquamaingi@gmail.com>
2024-03-24 17:01:55 +0300

    Update files that should fail

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,69 +9,13 @@ from models.city import City
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'], strict_slashes=False)
-def hello():
-    """Prints out hello"""
-    greeting = "Hello HBNB!"
-    return greeting
-
-
-@app.route("/hbnb", methods=['GET'], strict_slashes=False)
-def hello_2():
-    """Prints out specified greeting"""
-    output = "HBNB"
-    return output
-
-
-@app.route("/c/<text>", methods=['GET'], strict_slashes=False)
-def flask_var(text):
-    """Outputs predefined route text"""
-    myvar = " ".join(text.split("_"))
-    return (f"C {myvar}")
-
-
-@app.route("/python", methods=['GET'], strict_slashes=False)
-@app.route("/python/<text>", methods=['GET'], strict_slashes=False)
-def default_flask_var(text="is cool"):
-    """Outputs predefined route text"""
-    myvar = " ".join(text.split("_"))
-    return (f"Python {myvar}")
-
-
-@app.route("/number/<n>", methods=['GET'], strict_slashes=False)
-def is_var_num(n):
-    """Outputs predefined route number"""
-    if n.isdigit():
-        return f"{n} is a number"
-    else:
-        abort(404)
-
-
-@app.route("/number_template/<n>", methods=['GET'], strict_slashes=False)
-def temp_num(n):
-    """Outputs predefined route number"""
-    if n.isdigit():
-        return render_template("5-number.html", n=n)
-    else:
-        abort(404)
-
-
-@app.route("/number_odd_or_even/<n>", methods=['GET'], strict_slashes=False)
-def temp_logic_num(n):
-    """Outputs predefined route number"""
-    if n.isdigit():
-        return render_template("6-number_odd_or_even.html", n=n)
-    else:
-        abort(404)
-
-
 @app.route("/cities_by_states", methods=["GET"], strict_slashes=False)
 def list_states():
     """Returns a list of all states and their cities"""
     states = storage.all(State)
-    cities = storage.all(City)
+    # cities = storage.all(City)
     return render_template(
-        "8-cities_by_states.html", states=states, cities=cities)
+        "8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
