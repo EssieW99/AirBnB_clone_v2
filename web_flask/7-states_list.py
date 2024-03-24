80a7a39658085901af0639478963d7c3ffdc115a
Merch7x <aquamaingi@gmail.com>
2024-03-24 13:38:39 +0300

    Restructure teardown

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,12 +8,6 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(exception=None):
-    """Performs cleanup tasks by closing the db connection """
-    storage.close()
-
-
 @app.route("/states_list", methods=["GET"], strict_slashes=False)
 def list_states():
     """Returns a list of all states"""
@@ -22,8 +16,11 @@ def list_states():
     return render_template("7-states_list.html", states=states)
 
 
-teardown_db()
+@app.teardown_appcontext
+def teardown_db(exception=None):
+    """Performs cleanup tasks by closing the db connection """
+    storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
