e61ffafe4124ea0e036d798fec3c5fa61497289c
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-04-04 01:05:42 +0300

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,6 +6,6 @@ sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "Nginx configuration test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '24i\\tlocation hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '24i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
