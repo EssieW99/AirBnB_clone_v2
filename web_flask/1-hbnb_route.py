7e7c9661fcbdd1d48450cd5d99fc112550aa7eb5
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-04-21 20:13:25 +0300

    1. HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""Starts a minimal python app
-Creates routes for the app
-"""
+""" starts a Flask web application"""
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'], strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
-    """Prints out hello to the root route of the flask app"""
-    greeting = "Hello HBNB!"
-    return greeting
+    """ returns Hello HBNB!"""
+    return "Hello HBNB!"
 
 
-@app.route("/hbnb", methods=['GET'], strict_slashes=False)
-def hello_2():
-    """Prints out specified greeting at the route /hbnb"""
-    output = "HBNB"
-    return output
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return "HBNB"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
