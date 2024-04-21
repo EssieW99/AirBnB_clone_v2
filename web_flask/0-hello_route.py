bd181d6ccb297dd408ae4ee4b40aeb7fb7670884
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-04-21 20:07:18 +0300

    0. Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""Starts a minimal python app
-Creates routes for the app
-"""
+""" starts a Flask web application"""
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'], strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
-    """Prints out hello to the root route of the flask app"""
-    greeting = "Hello HBNB!"
-    return greeting
+    """ returns Hello HBNB!"""
+    return "Hello HBNB!"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
