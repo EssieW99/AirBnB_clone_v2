09de4cf5c28bf8d64502b76a62b7e971f1594c35
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-04-27 22:02:23 +0300

    3. Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,34 +1,86 @@
 #!/usr/bin/python3
-"""Creates an archive and uploads it to the server"""
-from fabric import exceptions
-from fabric.api import env, runs_once, local
-from time import strftime
+"""
+distributes an archive to your web servers
+"""
+from datetime import datetime
+from fabric.api import *
+from os.path import basename, exists, splitext
 
-upload = __import__('2-do_deploy_web_static').do_deploy
+env.hosts = ['34.203.29.35', '100.25.3.77']
+env.user = 'ubuntu'
+env.key = '~/.ssh/school'
 
-env.hosts = ["54.237.82.60", "100.27.4.209"]
-env.user = "ubuntu"
 
-
-@runs_once
 def do_pack():
-    """Archive the web_static folder"""
-    filename = strftime("%Y%m%d%H%M%S")
-    archive_path = "versions/web_static_{}.tgz".format(filename)
+    """ generates a .tgz archive from web_static folder"""
 
     try:
-        local("mkdir -p versions")
-        local("tar -czvf {} web_static/".format(archive_path))
-        return archive_path
-    except exceptions.CommandExecutionError as e:
-        print(e)
+        date = datetime.now().strftime('%Y%m%d%H%M%S')
+        if not exists('versions'):
+            local('mkdir versions')
+        filename = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(filename))
+        return filename
+    except Exception:
         return None
 
 
-def deploy():
-    """Creates an archive and uploads to servers"""
-    archive = do_pack()
-    if archive is None:
+def do_deploy(archive_path):
+    """ distributes an archive to your web servers"""
+
+    """ check if path exists"""
+    path_exists = exists(archive_path)
+    if path_exists is False:
         return False
-    deployment = upload(archive)
-    return deployment
+
+    try:
+        """ get last component of the path"""
+        filename = basename(archive_path)
+
+        """ split filename into base name and extension"""
+        name_no_ext, _ = splitext(filename)
+
+        """ where archive should be uncompressed"""
+        folder = f"/data/web_static/releases/{name_no_ext}"
+
+        """ upload archive to /tmp/ directory"""
+        put(archive_path, '/tmp/')
+
+        run('mkdir -p {}'.format(folder))
+
+        """ uncompress the archive from where it is to where you want to be"""
+        run('tar -xzf /tmp/{} -C {}/'.format(filename, folder))
+
+        run('rm /tmp/{}'.format(filename))
+
+        """
+        move all files and directories within web_static directory to the
+        root of the deployment folder
+        """
+        run('mv {}/web_static/* {}'.format(folder, folder))
+
+        """ delete the web_static directory"""
+        run('rm -rf {}/web_static'.format(folder))
+
+        """ delete the symbolic link from the servers"""
+        run('rm -rf /data/web_static/current')
+
+        """
+        create a new symbolic link linked to the new version of the code
+        """
+        run(f'ln -s {folder}/ /data/web_static/current')
+
+        return True
+    except Exception:
+        return False
+
+
+def deploy():
+    """
+    creates and distributes an archive to your web servers
+    """
+
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
